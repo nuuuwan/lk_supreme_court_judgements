e3b8f8ed8a6b2ad62281d17276564253374862b0
nuuuwan <nuuuwan@gmail.com>
2025-09-16 19:28:47 +0530

    Added remote_data_url

--- src/supreme/JudgementDoc.py
+++ src/supreme/JudgementDoc.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import cached_property
 
 from utils import Log
 
@@ -23,4 +24,26 @@ class JudgementDoc(JudgementDocWebMixin, AbstractDoc):
     def get_doc_class_description(cls) -> str:
         return (
             f"Judgements of the [Supreme Court of Sri Lanka]({cls.BASE_URL})."
+        )
+
+    @classmethod
+    def get_remote_data_url_base(cls) -> str:
+        return "/".join(
+            [
+                "https://github.com",
+                "nuuuwan",
+                "lk_supreme_court_judgements",
+                "tree",
+                "data",  # branch-name
+            ]
+        )
+
+    @cached_property
+    def remote_data_url(self) -> str:
+        return "/".join(
+            [
+                self.get_remote_data_url_base(),
+                self.__class__.get_dir_docs_for_cls_relative(),
+                self.dir_doc_relative_to_class,
+            ]
         )
