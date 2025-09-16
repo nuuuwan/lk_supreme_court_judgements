dfc35e5032f8856e5792277e6a0e0394a547128f
nuuuwan <nuuuwan@gmail.com>
2025-09-16 17:46:36 +0530

    Deleted files

--- src/supreme/__init__.py
+++
@@ -1,4 +0,0 @@
-# supreme (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from supreme.JudgementDoc import JudgementDoc
--- src/utils_future/__init__.py
+++
@@ -1,9 +0,0 @@
-# utils_future (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from utils_future.BigJSONFile import BigJSONFile
-from utils_future.Chunker import Chunker
-from utils_future.FileFuture import FileFuture
-from utils_future.Markdown import Markdown
-from utils_future.pdf_file import PDFFile, PDFTextMixin
-from utils_future.WWW import WWW
