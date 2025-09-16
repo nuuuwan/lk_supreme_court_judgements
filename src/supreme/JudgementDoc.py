1e4412b63e28935249034e28670a01b9f9e4b0c2
nuuuwan <nuuuwan@gmail.com>
2025-09-16 20:18:09 +0530

    inherit from parent

--- src/supreme/JudgementDoc.py
+++ src/supreme/JudgementDoc.py
@@ -17,35 +17,9 @@ class JudgementDoc(JudgementDocWebMixin, AbstractDoc):
     BASE_URL = "https://supremecourt.lk/judgements/"
 
     @classmethod
-    def get_doc_class_label(cls) -> str:
-        return "lk_supreme_court_judgements"
-
-    @classmethod
     def get_doc_class_description(cls) -> str:
         return (
             f"Judgements of the [Supreme Court of Sri Lanka]({cls.BASE_URL})."
-        )
-
-    @classmethod
-    def get_remote_data_url_base(cls) -> str:
-        return "/".join(
-            [
-                "https://github.com",
-                "nuuuwan",
-                "lk_supreme_court_judgements",
-                "tree",
-                "data",  # branch-name
-            ]
-        )
-
-    @cached_property
-    def remote_data_url(self) -> str:
-        return "/".join(
-            [
-                self.get_remote_data_url_base(),
-                self.__class__.get_dir_docs_for_cls_relative(),
-                self.dir_doc_relative_to_class,
-            ]
         )
 
     @cached_property
