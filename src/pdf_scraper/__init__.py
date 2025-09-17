789e279578a3ef0c0fef953ea735cc2fa54cda60
nuuuwan <nuuuwan@gmail.com>
2025-09-17 11:45:27 +0530

    [push_to_clients] Updated pdf_scraper

---
+++ src/pdf_scraper/__init__.py
@@ -0,0 +1,14 @@
+# pdf_scraper (auto generate by build_inits.py)
+# flake8: noqa: F408
+
+from pdf_scraper.abstract_doc import (AbstractDoc, AbstractDocBase,
+                                      AbstractDocChartDocsByYearMixin,
+                                      AbstractDocExtendedDataMixin,
+                                      AbstractDocGeneratorMixin,
+                                      AbstractDocHuggingFaceMixin,
+                                      AbstractDocMetadataMixin,
+                                      AbstractDocPipelineCleanupMixin,
+                                      AbstractDocPipelineExtendedDataMixin,
+                                      AbstractDocPipelineMetadataMixin,
+                                      AbstractDocPipelineMixin,
+                                      AbstractDocReadMeMixin)
