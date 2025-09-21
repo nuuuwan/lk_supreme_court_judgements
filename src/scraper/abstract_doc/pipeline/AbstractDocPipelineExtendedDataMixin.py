f6e8901db20b79279df039494418b00edd94bec3
nuuuwan <nuuuwan@gmail.com>
2025-09-21 10:06:27 +0530

    [push_to_clients] Updated scraper

--- src/scraper/abstract_doc/pipeline/AbstractDocPipelineExtendedDataMixin.py
+++ src/scraper/abstract_doc/pipeline/AbstractDocPipelineExtendedDataMixin.py
@@ -1,21 +1,37 @@
 import os
 import time
+from typing import Generator
 
-from utils import Log
+from utils import Log, Parallel
 
 log = Log("AbstractDocPipelineExtendedDataMixin")
 
 
 class AbstractDocPipelineExtendedDataMixin:
+    EXTENDED_PROCESSING_BATCHS_SIZE = 4
+    MAX_THREADS = 4
+
+    @classmethod
+    def gen_doc_batch_list(cls) -> Generator[list, None, None]:
+        all_docs = cls.list_all()
+        for i in range(0, len(all_docs), cls.EXTENDED_PROCESSING_BATCHS_SIZE):
+            i_start = i
+            i_end = min(i + cls.EXTENDED_PROCESSING_BATCHS_SIZE, len(all_docs))
+            yield all_docs[i_start:i_end]
+
+    @classmethod
+    def process_doc_batch(cls, doc_batch):
+        return Parallel.map(
+            lambda doc: doc.scrape_extended_data_for_doc(),
+            doc_batch,
+            max_threads=cls.MAX_THREADS,
+        )
+
     @classmethod
     def scrape_all_extended_data(cls, max_dt):
         t_start = time.time()
-        for doc in cls.list_all():
-            try:
-                doc.scrape_extended_data_for_doc()
-            except Exception as e:
-                log.error(f"Error scraping extended data for {doc}: {e}")
-
+        for doc_batch in cls.gen_doc_batch_list():
+            cls.process_doc_batch(doc_batch)
             dt = time.time() - t_start
             if dt > max_dt:
                 log.info(f"🛑 Stopping. {dt:,.1f}s > {max_dt:,}s")
