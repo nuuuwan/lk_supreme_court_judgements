7f36be82c41bc195f0bffb41521f0859cf744951
nuuuwan <nuuuwan@gmail.com>
2025-09-21 10:20:30 +0530

    [push_to_clients] Updated scraper

--- src/scraper/abstract_doc/pipeline/AbstractDocPipelineExtendedDataMixin.py
+++ src/scraper/abstract_doc/pipeline/AbstractDocPipelineExtendedDataMixin.py
@@ -8,19 +8,19 @@ log = Log("AbstractDocPipelineExtendedDataMixin")
 
 
 class AbstractDocPipelineExtendedDataMixin:
-    EXTENDED_PROCESSING_BATCHS_SIZE = 4
-    MAX_THREADS = 4
 
     @classmethod
-    def gen_doc_batch_list(cls) -> Generator[list, None, None]:
+    def gen_doc_batches_for_scrape_extended_data(
+        cls,
+    ) -> Generator[list, None, None]:
         all_docs = cls.list_all()
-        for i in range(0, len(all_docs), cls.EXTENDED_PROCESSING_BATCHS_SIZE):
+        for i in range(0, len(all_docs), cls.BATCH_SIZE):
             i_start = i
-            i_end = min(i + cls.EXTENDED_PROCESSING_BATCHS_SIZE, len(all_docs))
+            i_end = min(i + cls.BATCH_SIZE, len(all_docs))
             yield all_docs[i_start:i_end]
 
     @classmethod
-    def process_doc_batch(cls, doc_batch):
+    def process_doc_batch_for_scrape_extended_data(cls, doc_batch):
         return Parallel.map(
             lambda doc: doc.scrape_extended_data_for_doc(),
             doc_batch,
@@ -29,9 +29,12 @@ class AbstractDocPipelineExtendedDataMixin:
 
     @classmethod
     def scrape_all_extended_data(cls, max_dt):
+        log.debug(f"BATCH_SIZE={cls.BATCH_SIZE}")
+        log.debug(f"MAX_THREADS={cls.MAX_THREADS}")
+
         t_start = time.time()
-        for doc_batch in cls.gen_doc_batch_list():
-            cls.process_doc_batch(doc_batch)
+        for doc_batch in cls.gen_doc_batches_for_scrape_extended_data():
+            cls.process_doc_batch_for_scrape_extended_data(doc_batch)
             dt = time.time() - t_start
             if dt > max_dt:
                 log.info(f"🛑 Stopping. {dt:,.1f}s > {max_dt:,}s")
